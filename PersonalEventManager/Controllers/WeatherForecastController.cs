4545b16cf8a48b061a4e3ecfaed58b782b8b0de8
SAIFULLAH BHATTI <saifullah.bhatti@cbc.ca>
2021-01-17 15:23:10 -0500

    adding another weather state

--- PersonalEventManager/Controllers/WeatherForecastController.cs
+++ PersonalEventManager/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace PersonalEventManager.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "extremeHot"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
